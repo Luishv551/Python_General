e0fe02b8490d27ec3424fbbcce09f09fb1c1f123
Luis Santos <luishv551@gmail.com>
2024-07-04 22:14:38 -0300

    Beecrowd

---
+++ AlgoritmosBeecrowd/1018_cedulas.py
@@ -0,0 +1,29 @@
+reais = int(input())
+reais_f = reais
+
+notas100 = reais // 100
+reais = reais % 100
+
+notas50 = reais // 50
+reais = reais % 50
+
+notas20 = reais // 20
+reais = reais % 20
+
+notas10 = reais // 10
+reais = reais % 10
+
+notas5 = reais // 5
+reais = reais % 5
+
+notas2 = reais // 2
+reais = reais % 2
+
+print(reais_f)
+print(f"{notas100} nota(s) de R$ 100,00")
+print(f"{notas50} nota(s) de R$ 50,00")
+print(f"{notas20} nota(s) de R$ 20,00")
+print(f"{notas10} nota(s) de R$ 10,00")
+print(f"{notas5} nota(s) de R$ 5,00")
+print(f"{notas2} nota(s) de R$ 2,00")
+print(f"{reais} nota(s) de R$ 1,00")
