19e6fc999a60602c2a414af253b6bd9d1d33aa58
Luis Santos <luishv551@gmail.com>
2024-07-20 20:03:33 -0300

    exercicios beecrowd

--- AlgoritmosBeecrowd/1171_freqnumeros.py
+++ AlgoritmosBeecrowd/1171_freqnumeros.py
@@ -21,27 +21,5 @@ for numero in numeros_ordenados:
     print(f"{numero} aparece {numeros_processados[numero]} vez(es)")
 
 
-"""
-
-numeros_processados = []
-
-for numero in numeros:
-
-    if numero not in numeros_processados:
-
-        contagem = 0
-
-        for elemento in numeros:
-
-            if numero == elemento: # if numeros[numero] == elemento:
-
-                contagem = contagem + 1
-
-        print(f"{numero} aparece {contagem} vez(es)")
-        numeros_processados.append(numero)
-
-"""
-
-
 
      
---
+++ AlgoritmosBeecrowd/ppt_1873.py
@@ -0,0 +1,49 @@
+"""
+Tesoura corta papel
+Papel cobre pedra
+Pedra derruba lagarto
+Lagarto adormece Spock
+Spock derrete tesoura
+Tesoura prende lagarto
+Lagarto come papel
+Papel refuta Spock
+Spock vaporiza pedra
+Pedra quebra tesoura
+"""
+
+#REGRAS DA VITORIA 
+tesoura = ["papel", "lagarto"]
+papel = ["pedra", "spock"]
+pedra = ["lagarto", "tesoura"]
+lagarto = ["spock", "papel"]
+spock = ["tesoura", "pedra"]
+
+def determinar_vencedor(rajesh, sheldon):
+    
+    if rajesh == sheldon:
+        return "empate"
+    
+    if rajesh == "tesoura" and sheldon in tesoura:
+        return "rajesh"
+    elif rajesh == "papel" and sheldon in papel:
+        return "rajesh"
+    elif rajesh == "pedra" and sheldon in pedra:
+        return "rajesh"
+    elif rajesh == "lagarto" and sheldon in lagarto:
+        return "rajesh"
+    elif rajesh == "spock" and sheldon in spock:
+        return "rajesh"
+    else:
+        return "sheldon"
+
+
+n_partidas = int(input())
+
+
+for _ in range(n_partidas):
+
+    rajesh, sheldon = input().split()
+    
+    resultado = determinar_vencedor(rajesh, sheldon)
+
+    print(resultado)
